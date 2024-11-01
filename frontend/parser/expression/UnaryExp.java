22639a35e10a9932eb9dfd9da9e61632145f54fd
WZQ <13875445+bush04@user.noreply.gitee.com>
2024-11-01 18:57:03 +0800

    UnaryExp → UnaryOp UnaryExp这条规则不应拆分子类而应把UnaryOp作为UnaryExp的属性

--- frontend/parser/expression/UnaryExp.java
+++ frontend/parser/expression/UnaryExp.java
@@ -5,11 +5,15 @@ import frontend.parser.Parser;
 import frontend.parser.SyntaxNode;
 import frontend.parser.expression.unaryexps.FunctionCall;
 import frontend.parser.expression.unaryexps.PrimaryExp;
-import frontend.parser.expression.unaryexps.UnaryOpExp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnaryExp implements SyntaxNode<UnaryExp> {
+    // 相邻两个 UnaryOp 不能相同
+    // UnaryOp 为 '!' 只能出现在条件表达式中
+    List<UnaryOp> unaryOps = new ArrayList<>();
     UnaryExp unaryExp; // unaryexps包里的子类之一
 
     public UnaryExp getUnaryExp() {
@@ -20,14 +24,18 @@ public class UnaryExp implements SyntaxNode<UnaryExp> {
     public UnaryExp parse() throws IOException {
         Token.TokenType tokenType = Parser.currentSymbol().getTokenType();
 
+        while (tokenType == Token.TokenType.PLUS
+                || tokenType == Token.TokenType.MINU
+                || tokenType == Token.TokenType.NOT) {
+            unaryOps.add(new UnaryOp().parse());
+            Parser.getSymbol();
+            tokenType = Parser.currentSymbol().getTokenType();
+        }
+
         if (tokenType == Token.TokenType.LPARENT
         || tokenType == Token.TokenType.INTCON
         || tokenType == Token.TokenType.CHRCON) {
             unaryExp = new PrimaryExp().parse();
-        } else if (tokenType == Token.TokenType.PLUS
-        || tokenType == Token.TokenType.MINU
-        || tokenType == Token.TokenType.NOT) {
-            unaryExp = new UnaryOpExp().parse();
         }
         // 此外tokenType只会是IDENFR，但要通过超前看两个token区分为两种情况
         else if (tokenType == Token.TokenType.IDENFR) {
